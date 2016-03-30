13738e324d24ee77df2d3ba65cc5b9e0e003b4e6
Klemen Kenda <klemen.kenda@ijs.si>
2016-03-30 03:31:00 +0200

    Added Log pusher.

---
+++ LogPush/inc/config.inc.php
@@ -0,0 +1,28 @@
+<?PHP
+//---------------------------------------------------------------------
+// FILE: config.inc.php
+// AUTHOR: Klemen Kenda
+// DESCRIPTION: Log Pusher for Seensy
+// DATE: 22/03/2016
+// HISTORY:
+//---------------------------------------------------------------------
+
+// mysql config -------------------------------------------------------
+$mysql_user = "root";
+$mysql_pass = "";
+$mysql_host = "localhost";
+$mysql_dbase = "seensy";
+
+// mail config --------------------------------------------------------
+$webmaster_mail = "klemen.kenda@seensy.me";
+
+// filesystem config --------------------------------------------------
+$filesystem_root = "D:\\www\\";
+
+// miner config -------------------------------------------------------
+$miner["url"] = "http://127.0.0.1";
+$miner["port"] = 9201;
+$miner["stream_timeout"] = 20;
+$miner["socket_timeout"] = 10;
+
+?>
---
+++ LogPush/push.php
@@ -0,0 +1,63 @@
+<?PHP
+//---------------------------------------------------------------------
+// FILE: push.php
+// AUTHOR: Klemen Kenda
+// DESCRIPTION: Archive Push script
+// DATE: 01/12/2013
+// HISTORY:
+//---------------------------------------------------------------------
+
+// initialization 
+// DEBUG
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set("display_errors", 1);
+
+// get request variables
+// import_request_variables("gPC");
+extract($_GET); extract($_POST); extract($_COOKIE);
+
+// includes
+include("inc/http.inc.php");
+include("inc/config.inc.php");
+
+
+function isJson($string) {
+  json_decode($string);
+  return (json_last_error() == JSON_ERROR_NONE);
+}
+
+function pushFile($filename) {
+	global $miner; 
+	
+	$fp = fopen("./logs_push/" . $filename, "r");
+		$i = 0;
+	while (($JSON = fgets($fp, 4000000)) !== false) {
+		$i++;
+		// echo $JSON . "\n\n";
+		// create request to EnStreaM
+		if (isJSON($JSON)) {
+		  $url = $miner["url"] . ":" . $miner["port"] . "/enstream/add-measurement?data=" . urlencode($JSON);
+		  // uncomment to do the real push!
+		  $HTML = getURL($url);		
+		  echo $HTML . "\n\n";		  
+		}
+		echo $i . "           \r";
+    }
+     
+    fclose($fp);		
+}
+
+// get log files from the logs folder
+$dp = opendir("./logs_push");
+
+// scan and push all log files
+while (false !== ($entry = readdir($dp))) {
+	if (is_file("./logs_push/" . $entry)) {
+		echo "$entry ----------------------------------------\n";
+		pushFile($entry);
+	}	
+}
+
+closedir($dp);
+
+?>
